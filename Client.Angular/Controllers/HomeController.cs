2c8adabc0d91ac1b76d6f7f8a2c11b5acbf2dc3a
Artem Gorbatuk <gorbatuk.am@gmail.com>
2022-07-23 12:18:08 +0200

    Task-001 add angular project

---
+++ Client.Angular/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Angular.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
